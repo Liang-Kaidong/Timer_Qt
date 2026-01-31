34c7c5c5d2a56e004c3cfe516121882d09632b19
Liang-Kaidong <226429965@qq.com>
2026-01-31 18:51:23 +0800

    Add files via upload

---
+++ widget.cpp
@@ -0,0 +1,83 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QDebug>
+#include <QDesktopWidget>
+#include <QStyle>
+#include <QRect>
+
+static int i, isstop = 0;
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    connect(&timer, SIGNAL(timeout()), this, SLOT(timeout_Slot()));
+    setWindowTitle("计时器");
+
+    QDesktopWidget *deskTopWidget = QApplication::desktop();
+    QRect deskRect = deskTopWidget->availableGeometry();
+
+    int appH = deskRect.height();
+    int appW = deskRect.width();
+    this->setFixedSize(appW, appH);
+    setGeometry(0, 0, appW, appH);
+
+
+    time.setHMS(0, 0, 0, 0);
+    ui->ShowTimeEdit->setText("00:00:00:000");
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::timeout_Slot()
+{
+    //qDebug("1000ms");
+    time = time.addMSecs(33);
+    ui->ShowTimeEdit->setText(time.toString("hh:mm:ss:zzz"));
+}
+
+void Widget::on_StartButton_clicked()
+{
+    timer.start(30);
+    ui->StartButton->setEnabled(false);
+    isstop = 0;
+}
+
+void Widget::on_StopButton_clicked()
+{
+    timer.stop();
+    ui->StartButton->setEnabled(true);
+    isstop = 1;
+}
+
+void Widget::on_ResetButton_clicked()
+{
+    timer.stop();
+    time.setHMS(0, 0, 0, 0);
+    ui->ShowTimeEdit->setText("00:00:00:000");
+    ui->BitEdit->clear();
+    i = 0;
+    ui->StartButton->setEnabled(true);
+    isstop = 1;
+}
+
+void Widget::on_BitButton_clicked()
+{
+    QString temp;
+
+    i = i + 1;
+    temp.sprintf("%d", i);
+    ui->BitEdit->append(temp);
+    ui->BitEdit->append(time.toString("hh:mm:ss:zzz"));
+
+    if (isstop == 1) {
+        ui->StartButton->setEnabled(true);
+    } else {
+        ui->StartButton->setEnabled(false);
+    }
+
+}
---
+++ widget.h
@@ -0,0 +1,38 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QTime>
+#include <QTimer>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+    QTimer timer;
+    QTime time;
+
+private slots:
+    void on_StartButton_clicked();
+
+    void timeout_Slot();
+
+    void on_StopButton_clicked();
+
+    void on_ResetButton_clicked();
+
+    void on_BitButton_clicked();
+
+private:
+    Ui::Widget *ui;
+};
+
+#endif // WIDGET_H
